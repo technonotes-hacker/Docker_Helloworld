63fdec97e06681ae51bbcc45de9452b53d3b2f86
sathishpy1808 <sathishpy1808@meet.sathishpy1808.org>
2023-09-26 08:03:03 +0530

    second commit for script change

--- script.sh
+++ script.sh
@@ -3,6 +3,15 @@
 # clone the repo
 git clone https://github.com/technonotes-hacker/Docker_Helloworld.git
 
+cd Docker_Helloworld
+
+REMOTE=origin
+BRANCH=master
+git fetch
+if [[ "$(git rev-parse $BRANCH)" != "$(git rev-parse "$REMOTE/$BRANCH")" ]]; then
+  # Run your script
+git pull
+
 # build the docker image 
 
 docker build -t sathishpy1808/Docker_Helloworld:latest .
@@ -13,3 +22,7 @@ docker push sathishpy1808/Docker_Helloworld
 #run the docker container 
 
 docker run -d -p 8090:80 Docker_Helloworld
+else
+	echo " No change on $REMOTE "
+fi
+
